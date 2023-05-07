60553b80ceaab708c90a37d3a9242aab1649f79b
EdenReuven <74300459+EdenReuven@users.noreply.github.com>
2023-05-07 20:04:43 +0300

    fix the convert to pdf

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -2,19 +2,14 @@ import React from "react";
 import "./Form.scss";
 import FieldList from "../filedList/FieldList";
 import Filed from "../filed/Filed";
-//import ConvertButton from "../convertButton/ConvertButton";
+import { upperCaseLabel , convertToPDF} from "../../utils/function";
 
 const Form = ({ data }) => {
-  const upperCaseLabel = (word) => {
-    let string = word.split("_");
-    let label = "";
-    for (let i = 0; i < string.length; i++) {
-      label =
-        label + " " + string[i].charAt(0).toUpperCase() + string[i].slice(1);
-    }
-    return label;
+  const handleConvert = () => {
+    const form = document.getElementById("formDiv");
+    convertToPDF(form);
   };
-  const handleConvert = () => {};
+
   return (
     <div className="form">
       <div className="convertBtn">
@@ -22,33 +17,35 @@ const Form = ({ data }) => {
           convert to pdf
         </button>
       </div>
-      {Object.keys(data).map((key) => {
-        if (Array.isArray(data[key])) {
-          const newArr = data[key];
-          return (
-            <div className="fieldContainer">
-              <h1>{upperCaseLabel(key)}</h1>
-              {newArr.map((obj, index) => (
-                <FieldList key={index} data={obj} />
-              ))}
-            </div>
-          );
-        } else if (typeof data[key] === "object" && data[key] !== null) {
-          let newObj = data[key];
-          return (
-            <div className="fieldContainer">
-              <h1>{upperCaseLabel(key)}</h1>
-              <FieldList data={newObj} />
-            </div>
-          );
-        } else {
-          return (
-            <div className="fieldContainer">
-              <Filed key={key} data={data[key]} name={key} />
-            </div>
-          );
-        }
-      })}
+      <div id="formDiv">
+        {Object.keys(data).map((key) => {
+          if (Array.isArray(data[key])) {
+            const newArr = data[key];
+            return (
+              <div className="fieldContainer">
+                <h1>{upperCaseLabel(key)}</h1>
+                {newArr.map((obj, index) => (
+                  <FieldList key={index} data={obj} />
+                ))}
+              </div>
+            );
+          } else if (typeof data[key] === "object" && data[key] !== null) {
+            let newObj = data[key];
+            return (
+              <div className="fieldContainer">
+                <h1>{upperCaseLabel(key)}</h1>
+                <FieldList data={newObj} />
+              </div>
+            );
+          } else {
+            return (
+              <div className="fieldContainer">
+                <Filed key={key} data={data[key]} name={key} />
+              </div>
+            );
+          }
+        })}
+      </div>
     </div>
   );
 };
